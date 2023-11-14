2dc78f3526be995964af66c5bffed4e448e8e553
Alexaanderm728 <Alexaanderm728@live.com>
2023-11-14 16:14:47 +0100

    Configure React app for deployment to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import JSXRules from './JSXRules.tsx';
+import FancyTable from './FancyTable.tsx';
+import Multiple from './Multiple.tsx';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <JSXRules text="This is a blockquote with JSX Rules." />
+      <h2>Fancy Table</h2>
+      <FancyTable n={5}/>
+      <Multiple name='Multiple Component' number={4}/>
     </div>
   );
 }
---
+++ src/JSXRules.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+const JSXRules: FC<{ text: string }> = ({ text }) => {
+  return <blockquote>{text}</blockquote>;
+};
+
+export default JSXRules;
